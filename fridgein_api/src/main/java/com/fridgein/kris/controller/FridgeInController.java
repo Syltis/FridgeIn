b7bad552d241cbefcb87f2903f94b99f8e0f497d
Kristoffer Sylte Dahl <krsylte@gmail.com>
2019-03-09 14:13:49 +0100

    Started on Axios api connection

--- fridgein_api/src/main/java/com/fridgein/kris/controller/FridgeInController.java
+++ fridgein_api/src/main/java/com/fridgein/kris/controller/FridgeInController.java
@@ -7,6 +7,7 @@ import com.fridgein.kris.service.StockItemService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class FridgeInController {
@@ -38,5 +39,15 @@ public class FridgeInController {
     @GetMapping("/stockitems")
     public List<StockItem> getAllStockItems() { return stockItemService.getAllStockItems(); }
 
+    // Rest service-testing -> https://spring.io/guides/gs/rest-service-cors/
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @CrossOrigin(origins = "http://localhost:9000")
+    @GetMapping("/greeting")
+    public FoodItem greeting(@RequestParam(required = false, defaultValue = "World") String name) {
+        System.out.println("==== in greeting ====");
+        return new FoodItem(counter.incrementAndGet(), String.format(template, name), "test");
+    }
 
 }
--- fridgein_fe/src/App.vue
+++ fridgein_fe/src/App.vue
@@ -1,30 +1,60 @@
 <!-- npm run serve -- --port8090 -->
 
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<img alt="Vue logo" src="./assets/logo.png">
+		<ul v-if="fooditems && fooditems.length">
+			<li v-for="fooditem of fooditems">
+				<p><strong>{{fooditem.name}}</strong></p>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import axios from 'axios'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        data() {
+            return {
+                ids: [],
+                names: []
+            }
+        },
+
+        created() {
+            axios.get("http://localhost:8080/greeting")
+                .then(response => {
+                    // JSON responses are automatically parsed.
+                    this.fooditems = response.data
+                })
+        }
+        // async / await version (created() becomes async created())
+        //
+        // try {
+        //   const response = await axios.get(`http://jsonplaceholder.typicode.com/posts`)
+        //   this.posts = response.data
+        // } catch (e) {
+        //   this.errors.push(e)
+        // }
+    }
+    /*import HelloWorld from './components/HelloWorld.vue'
+
+	export default {
+	  name: 'app',
+	  components: {
+		HelloWorld
+	  }
+	}*/
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
 </style>
