8534fcaa91f188acbbd1b56b70a58608b22d83b4
Kristoffer Sylte Dahl <krsylte@gmail.com>
2019-03-13 12:02:56 +0100

    Added startPage image, debugged the AddStockItemForm and the footer.

--- fridgein_fe/src/components/layout/BottomBar.vue
+++ fridgein_fe/src/components/layout/BottomBar.vue
@@ -1,16 +1,11 @@
 <template>
 	<v-footer height="auto" dark>
-		<v-card flat tile class="flex">
-			<v-card-title class="cyan darken-1">
-				<v-card-text class="text-md-center">
-				Built with Spring Boot as backend API currently using a H2 in-memory database, and Vue.js as frontend
-				using Axios and Vuetify.
-				</v-card-text>
-				<v-spacer></v-spacer>
+		<v-card class="flex">
+			<v-card-title class="green justify-center" align="center">
+				Make sure to check us out on social media
 			</v-card-title>
 			<v-card-actions class="grey darken-2 justify-center">
 				&copy;2018 — Fridge<strong>In</strong>
-				<v-icon>home</v-icon>
 			</v-card-actions>
 		</v-card>
 	</v-footer>
