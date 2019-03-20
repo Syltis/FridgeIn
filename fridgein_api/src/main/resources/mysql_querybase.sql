157935f529443bc31ee69e17fb088a93b4877a99
Kristoffer Sylte Dahl <krsylte@gmail.com>
2019-03-20 15:17:57 +0100

    Users can now add to they're stock. Food is also added to the fooditem-table.

--- fridgein_api/src/main/resources/mysql_querybase.sql
+++ fridgein_api/src/main/resources/mysql_querybase.sql
@@ -1,6 +1,13 @@
+# SELECT statements
 select * from stockitem;
 select * from fooditem;
 
+select fooditem_id from fooditem where type = 1;
+delete from stockitem where type = 't';
 delete from fooditem where type = 't';
+
+# CONSTRAINT statements
+
+alter table fooditem change fooditem_id id bigint;
 
 SET SQL_SAFE_UPDATES = 0;
