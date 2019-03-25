d9574832881d34a998c53bba2cb99e3fe30ceb4e
Kristoffer Sylte Dahl <krsylte@gmail.com>
2019-03-25 21:09:11 +0100

    Further debugging of entities and AddStockItemForm.

--- fridgein_fe/src/components/ui/AddStockItemForm.vue
+++ fridgein_fe/src/components/ui/AddStockItemForm.vue
@@ -1,41 +1,72 @@
 <template id="addStockItemForm" xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
-        <v-card class="formCard">
-            <v-layout row wrap>
+    <v-card class="formCard">
+        <v-layout row wrap>
 
-                <!-- Row one -->
-                <v-flex xs6>
-                    <h1 class="display-1 font-weight-thin">
-                        Add food to your stock
-                    </h1>
-                    <v-spacer></v-spacer>
-                </v-flex>
-                <v-flex x4>
-                    <food-item-select></food-item-select>
-                </v-flex>
+            <!-- Row one -->
+            <v-flex xs6>
+                <h1 class="display-1 font-weight-thin">
+                    Add food to your stock
+                </h1>
+                <v-spacer></v-spacer>
+            </v-flex>
+            <v-flex x4>
+                <food-item-select></food-item-select>
+            </v-flex>
 
-                <!-- Row two -->
-                <v-flex xs5 class="formFlex">
-                    <v-text-field
-                            v-model="stockItemName"
-                            label="Food Name"
-                            :counter="20"
-                            required
-                    ></v-text-field>
-                </v-flex>
-                <v-spacer xs2></v-spacer>
-                <v-flex xs5 class="formFlex">
-                    <v-text-field
-                            v-model="stockItemType"
-                            label="Type"
-                            :counter="20"
-                            required
-                    ></v-text-field>
-                </v-flex>
+            <!-- Row two -->
+            <v-flex xs5 class="formFlex">
+                <v-text-field
+                        v-model="stockItemName"
+                        label="Food Name"
+                        :counter="20"
+                        required
+                ></v-text-field>
+            </v-flex>
+            <v-spacer xs2></v-spacer>
+            <v-flex xs5 class="formFlex">
+                <v-text-field
+                        v-model="stockItemType"
+                        label="Type"
+                        :counter="20"
+                        required
+                ></v-text-field>
+            </v-flex>
 
-                <!-- Row three -->
-                <v-flex xs4 class="formFlex">
+            <!-- Row three -->
+            <v-flex xs4 class="formFlex">
+                <v-dialog
+                        v-model="modal"
+                        ref="dialog"
+                        lazy
+                        full-width
+                        width="290px"
+                >
+                    <template v-slot:activator="{ on }">
+                        <v-text-field
+                                v-model="purchaseDate"
+                                label="Purchase Date"
+                                prepend-icon="event"
+                                readonly
+                                v-on="on"
+                        ></v-text-field>
+                    </template>
+                    <v-date-picker v-model="purchaseDate" @input="modal = false"
+                                   scrollable></v-date-picker>
+                </v-dialog>
+            </v-flex>
+            <v-spacer xs1></v-spacer>
+            <v-flex xs2 class="formFlex">
+                <v-checkbox
+                        label="Expirable"
+                        @click.native="atChecked"
+                        id="expirationCheckBox"
+                ></v-checkbox>
+            </v-flex>
+            <v-spacer xs1></v-spacer>
+            <v-flex xs4 class="formFlex">
+                <div v-if="expirationCheckBox">
                     <v-dialog
-                            v-model="modal"
+                            v-model="modal2"
                             ref="dialog"
                             lazy
                             full-width
@@ -43,65 +74,34 @@
                     >
                         <template v-slot:activator="{ on }">
                             <v-text-field
-                                    v-model="purchaseDate"
-                                    label="Purchase Date"
+                                    v-model="expirationDate"
+                                    label="Expiration Date"
                                     prepend-icon="event"
                                     readonly
                                     v-on="on"
                             ></v-text-field>
                         </template>
-                        <v-date-picker v-model="purchaseDate" @input="modal = false"
+                        <v-date-picker v-model="expirationDate" @input="modal2 = false"
                                        scrollable></v-date-picker>
                     </v-dialog>
-                </v-flex>
-                <v-spacer xs1></v-spacer>
-                <v-flex xs2 class="formFlex">
-                    <v-checkbox
-                            label="Expirable"
-                            @click.native="atChecked"
-                            id="expirationCheckBox"
-                    ></v-checkbox>
-                </v-flex>
-                <v-spacer xs1></v-spacer>
-                <v-flex xs4 class="formFlex">
-                    <div v-if="expirationCheckBox">
-                        <v-dialog
-                                v-model="modal2"
-                                ref="dialog"
-                                lazy
-                                full-width
-                                width="290px"
-                        >
-                            <template v-slot:activator="{ on }">
-                                <v-text-field
-                                        v-model="expirationDate"
-                                        label="Expiration Date"
-                                        prepend-icon="event"
-                                        readonly
-                                        v-on="on"
-                                ></v-text-field>
-                            </template>
-                            <v-date-picker v-model="expirationDate" @input="modal2 = false"
-                                           scrollable></v-date-picker>
-                        </v-dialog>
-                    </div>
-                </v-flex>
+                </div>
+            </v-flex>
 
-                <!-- Row four -->
-                <v-flex xs12>
-                    <v-btn @click="submitStockItem"
-                           :disabled="!valid"
-                           color="success"
-                    >
-                        submit
-                    </v-btn>
-                    <v-btn @click="reset" color="error">clear</v-btn>
-                    <span class="font-weight-bold" v-if="stockItemSuccess">
+            <!-- Row four -->
+            <v-flex xs12>
+                <v-btn @click="submitStockItem2"
+                       :disabled="!valid"
+                       color="success"
+                >
+                    submit
+                </v-btn>
+                <v-btn @click="reset" color="error">clear</v-btn>
+                <span class="font-weight-bold" v-if="stockItemSuccess">
                         Food added to stock!
                     </span>
-                </v-flex>
-            </v-layout>
-        </v-card>
+            </v-flex>
+        </v-layout>
+    </v-card>
 
 </template>
 
@@ -132,6 +132,18 @@
                 this.expirationCheckBox = !this.expirationCheckBox;
             },
 
+            async submitStockItem2() {
+                axios.post('http://localhost:8080/api/fooditem/post', {
+                    "stockItems": [
+                        {
+                            "purchaseDate": this.purchaseDate,
+                            "expirationDate": this.expirationDate
+                        }
+                    ],
+                    "name": this.stockItemName,
+                    "type": this.stockItemType
+                })
+            },
             async submitStockItem() {
                 // post foodItem
                 axios.post('http://localhost:8080/api/fooditem/post', {
