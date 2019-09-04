95b3b6b396e70aa3340e60e2154b8ec69c8fe249
Kristoffer Sylte Dahl <kristoffer.dahl@bouvet.no>
2019-09-04 15:22:42 +0200

    Added a ASP.NET Core version of the API. This will be the currently supported version.

---
+++ fridgein-api/Models/Food.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace fridgein_api.Models
+{
+    public partial class Food
+    {
+        public Food()
+        {
+            Stockitem = new HashSet<Stockitem>();
+        }
+
+        public int FoodId { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+
+        public virtual ICollection<Stockitem> Stockitem { get; set; }
+    }
+}
---
+++ fridgein-api/Models/Stockitem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace fridgein_api.Models
+{
+    public partial class Stockitem
+    {
+        public int StockitemId { get; set; }
+        public int FoodId { get; set; }
+        public DateTime BoughtAt { get; set; }
+        public DateTime? ExpirationDate { get; set; }
+
+        public virtual Food Food { get; set; }
+    }
+}
