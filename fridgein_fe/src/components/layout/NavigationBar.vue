482b15b5e578352971a17267f6361f606c734931
Kristoffer Sylte Dahl <kristoffer.dahl@bouvet.no>
2019-09-05 15:28:31 +0200

    Debbuged axios-calls to the new API, debugged the StockItemList and FoodItemSelect. The chrome-console gives errors but its working.

--- fridgein_fe/src/components/layout/NavigationBar.vue
+++ fridgein_fe/src/components/layout/NavigationBar.vue
@@ -15,7 +15,7 @@
         <v-spacer></v-spacer>
         <v-toolbar-items >
             <v-btn flat to="/foodItems" class="btnlink">Stocks</v-btn>
-            <v-btn flat to="/recipes" class="btnlink">Recipies</v-btn>
+            <v-btn flat to="/recipes" class="btnlink">Recipes</v-btn>
             <v-btn flat to="contact" class="btnlink">Contact</v-btn>
             <v-btn flat id="loginBtn" class="btnlink">Log in</v-btn>
         </v-toolbar-items>
