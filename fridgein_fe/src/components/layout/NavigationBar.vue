fb57ec00d9eca76971965df27ca8d01437a57429
Syltis <krsylte@gmail.com>
2019-09-11 14:26:15 +0200

    Better responsiveness, styling, less old css, food+stock can now be deleted.

--- fridgein_fe/src/components/layout/NavigationBar.vue
+++ fridgein_fe/src/components/layout/NavigationBar.vue
@@ -1,48 +1,67 @@
 <template>
-    <v-toolbar app>
-        <v-toolbar-side-icon></v-toolbar-side-icon>
-        <v-toolbar-title class="headline text-uppercase">
-            <router-link class="btnlink" to="/">
-                <v-img
-                :src="logo"
-                height="40px"
-                width="120px"
-                ></v-img>
-                <!--<span class="font-weight-light">Fridge</span>
-                <span>In</span>-->
-            </router-link>
-        </v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-toolbar-items >
-            <v-btn flat to="/food" class="btnlink">Stocks</v-btn>
-            <v-btn flat to="/recipes" class="btnlink">Recipes</v-btn>
-            <v-btn flat to="contact" class="btnlink">Contact</v-btn>
-            <v-btn flat id="loginBtn" class="btnlink">Log in</v-btn>
-        </v-toolbar-items>
+  <div>
+    <v-toolbar class="hidden-sm-and-down">
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title class="headline text-uppercase">
+        <router-link class="btnlink" to="/">
+          <v-img :src="logo" height="40px" width="120px"></v-img>
+        </router-link>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn flat to="/food" class="btnlink">Stock</v-btn>
+        <v-btn flat to="/recipes" class="btnlink">Recipes</v-btn>
+        <v-btn flat to="contact" class="btnlink">Contact</v-btn>
+        <v-btn flat id="loginBtn" class="btnlink">Log in</v-btn>
+      </v-toolbar-items>
     </v-toolbar>
+    <v-menu
+      id="burger-menu"
+      class="hidden-md-and-up"
+      :offset-x="offset"
+      transition="slide-x-transition"
+      right
+    >
+      <v-toolbar-side-icon slot="activator"></v-toolbar-side-icon>
+      <v-list>
+        <v-list-tile v-for="item in menu" :key="item.text" :to="item.to">
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.text }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile>
+          <v-btn flat id="loginBtn" class="btnlink">Log in</v-btn>
+        </v-list-tile>
+      </v-list>
+    </v-menu>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Top",
-        data() {
-            return {
-                logo: require("@/assets/images/LogoMakr_3nTx2g.png")
-            }
-        }
-    }
+export default {
+  name: "Top",
+  data() {
+    return {
+      logo: require("@/assets/images/LogoMakr_3nTx2g.png"),
+      menu: [
+        { to: "/food", text: "Stock" },
+        { to: "/recipes", text: "Recipes" },
+        { to: "contact", text: "Contact" }
+      ],
+      offset: true
+    };
+  }
+};
 </script>
 
 <style scoped>
+.btnlink {
+  text-decoration: none;
+  color: inherit;
+}
 
-    .btnlink {
-        text-decoration: none;
-        color: inherit;
-    }
-
-    #loginBtn {
-        color: white;
-        background-color: #90CCF4;
-    }
-
+#loginBtn {
+  color: white;
+  background-color: #90ccf4;
+}
 </style>
