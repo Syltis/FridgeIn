0958c39573384eeb183d2e2ded097a8f5bd69c5d
Syltis <krsylte@gmail.com>
2019-09-11 10:12:42 +0200

    Added rule-based and much prettier validation. No nugatti though.

--- fridgein_fe/src/components/ui/AddStockItemForm.vue
+++ fridgein_fe/src/components/ui/AddStockItemForm.vue
@@ -1,95 +1,96 @@
 <template id="addStockItemForm" xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
   <v-card class="formCard">
     <v-form ref="form">
-      <v-layout row wrap>
-        <!-- Row one -->
-        <v-flex xs6 class="formFlex">
-          <h1 class="display-1 font-weight-thin">Add food to your stock</h1>
-          <v-spacer></v-spacer>
-        </v-flex>
-        <v-flex x4 class="formFlex">
-          <FoodSelect v-on:foodSelected="onFoodSelected" :key="foodComponentKey"></FoodSelect>
-        </v-flex>
+      <v-container grid-list-md>
+        <v-layout row wrap>
+          <!-- Row one -->
+          <v-flex xs12 class="formFlex">
+            <h1 class="display-1 font-weight-thin">Add food to your stock</h1>
+            <v-spacer></v-spacer>
+          </v-flex>
+          <v-flex xs12 md8 class="formFlex">
+            <FoodSelect v-on:foodSelected="onFoodSelected" :key="foodComponentKey"></FoodSelect>
+          </v-flex>
 
-        <!-- Row two -->
-        <v-flex xs5 class="formFlex">
-          <v-text-field v-model="stockItemName" label="Food Name" :counter="20" required></v-text-field>
-        </v-flex>
-        <v-spacer xs2></v-spacer>
-        <v-flex xs5 class="formFlex">
-          <v-text-field v-model="stockItemType" label="Type" :counter="20" required></v-text-field>
-        </v-flex>
+          <!-- Row two -->
+          <v-flex xs12 md5 class="formFlex">
+            <v-text-field v-model="stockItemName" label="Food Name" :rules="nameTypeRules" :counter="25" required></v-text-field>
+          </v-flex>
+          
+          <v-flex xs12 md5 class="formFlex">
+            <v-text-field v-model="stockItemType" label="Type" :rules="nameTypeRules" :counter="25" required></v-text-field>
+          </v-flex>
 
-        <!-- Row three -->
-        <v-flex xs4 class="formFlex">
-          <v-dialog v-model="modal" ref="dialog" lazy full-width width="290px">
-            <template v-slot:activator="{ on }">
-              <v-text-field
-                v-model="purchaseDate"
-                label="Purchase Date"
-                prepend-icon="event"
-                readonly
-                v-on="on"
-              ></v-text-field>
-            </template>
-            <v-date-picker v-model="purchaseDate" @input="modal = false" scrollable></v-date-picker>
-          </v-dialog>
-        </v-flex>
-        <v-spacer xs1></v-spacer>
-        <v-flex xs2 class="formFlex">
-          <v-checkbox label="Expirable" @click.native="atChecked" id="expirationCheckBox"></v-checkbox>
-        </v-flex>
-        <v-spacer xs1></v-spacer>
-        <v-flex xs4 class="formFlex">
-          <div v-if="expirationCheckBox">
-            <v-dialog v-model="modal2" ref="dialog" lazy full-width width="290px">
+          <!-- Row three -->
+          <v-flex xs12 md4 class="formFlex">
+            <v-dialog v-model="modal" ref="dialog" lazy full-width width="290px">
               <template v-slot:activator="{ on }">
                 <v-text-field
-                  v-model="expirationDate"
-                  label="Expiration Date"
+                  v-model="purchaseDate"
+                  label="Purchase Date"
                   prepend-icon="event"
                   readonly
                   v-on="on"
                 ></v-text-field>
               </template>
-              <v-date-picker v-model="expirationDate" @input="modal2 = false" scrollable></v-date-picker>
+              <v-date-picker v-model="purchaseDate" @input="modal = false" scrollable></v-date-picker>
             </v-dialog>
-          </div>
-        </v-flex>
+          </v-flex>
+          <v-spacer xs1 md1></v-spacer>
+          <v-flex xs12 md2 class="formFlex">
+            <v-checkbox label="Expirable" @click.native="atChecked" id="expirationCheckBox"></v-checkbox>
+          </v-flex>
+          <v-spacer xs1 md1></v-spacer>
+          <v-flex xs12 md4 class="formFlex">
+            <div v-if="expirationCheckBox">
+              <v-dialog v-model="modal2" ref="dialog" lazy full-width width="290px">
+                <template v-slot:activator="{ on }">
+                  <v-text-field
+                    v-model="expirationDate"
+                    label="Expiration Date"
+                    prepend-icon="event"
+                    readonly
+                    v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-date-picker v-model="expirationDate" @input="modal2 = false" scrollable></v-date-picker>
+              </v-dialog>
+            </div>
+          </v-flex>
 
-        <!-- Row four-->
-        <v-flex xs6 class="formFlex">
-          <v-slider
-            v-model="slider"
-            label="Amount"
-            class="align-center"
-            thumb-label="always"
-            :max="max"
-            :min="min"
-            hide-details
-          ></v-slider>
-        </v-flex>
+          <!-- Row four-->
+          <v-flex xs12 md6 class="formFlex">
+            <v-slider
+              v-model="slider"
+              label="Amount"
+              class="align-center"
+              thumb-label="always"
+              :max="max"
+              :min="min"
+              hide-details
+            ></v-slider>
+          </v-flex>
 
-        <!-- Row five -->
-        <v-flex xs12>
-          <v-btn @click="checkForm" :disabled="!valid" color="success">submit</v-btn>
-          <v-btn @click="reset" color="error">clear</v-btn>
-        </v-flex>
-        <v-flex xs4>
-          <v-card class="response-card" color="error" v-if="errors.length">
-            <b class="subheading responseText">Please correct the following errors</b>
-            <ul>
-              <li class="responseText" v-for="error in errors" v-bind:key="error">{{error}}</li>
-            </ul>
-          </v-card>
-          <v-card class="response-card" color="success" v-if="stockItemSuccess">
-            <b class="subheading responseText">
-              {{this.amountSaved}}
-              <b>{{this.itemSaved}}</b> added
-            </b>
-          </v-card>
-        </v-flex>
-      </v-layout>
+          <!-- Row five -->
+          <v-flex xs12 md12>
+            <v-btn @click="validate" :disabled="!valid" color="success">submit</v-btn>
+            <v-btn @click="reset" color="error">clear</v-btn>
+          </v-flex>
+          <v-flex xs12 md4 >
+            <v-card class="response-card" color="error" v-if="errors.length">
+              <ul>
+                <li class="responseText" v-for="error in errors" v-bind:key="error">{{error}}</li>
+              </ul>
+            </v-card>
+            <v-card class="response-card" color="success" v-if="stockItemSuccess">
+              <b class="subheading responseText">
+                {{this.amountSaved}}
+                <b>{{this.itemSaved}}</b> added
+              </b>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
     </v-form>
   </v-card>
 </template>
@@ -120,7 +121,12 @@ export default {
       modal: false,
       modal2: false,
       stockItemSuccess: false,
-      errors: []
+      errors: [],
+      nameTypeRules: [
+          v => !!v || 'Field is required',
+          v => /^[a-zæøåA-ZÆØÅ]+$/i.test(v) || 'Field can only contain alphabetical characters',
+          v => (v && v.length <= 25) || 'Field must be less than 25 characters'
+      ]
     };
   },
   computed: {
@@ -156,19 +162,14 @@ export default {
     valid() {
       return true;
     },
-    checkForm(e) {
-      this.stockItemSuccess = false;
-      this.errors = [];
-
-      if (this.stockItemName == "Nugatti") {
+    validate() {
+        if (this.stockItemName == "Nugatti") {
         this.errors.push("Nugatti skal ikke i kjøleskapet.");
         return;
       }
-
-      if (this.stockItemName && this.stockItemType) this.submitStockItem();
-      if (!this.stockItemName) this.errors.push("Name required");
-      if (!this.stockItemType) this.errors.push("Type required");
-      e.preventDefault();
+        if ( this.$refs.form.validate()) {
+            this.submitStockItem();
+        }
     },
     reset() {
       this.$refs.form.reset();
@@ -198,7 +199,7 @@ export default {
 }
 
 .response-card {
-  padding: 2%;
+  padding: 5%;
   margin: 5% 10% 0% 3%;
 }
 </style>
