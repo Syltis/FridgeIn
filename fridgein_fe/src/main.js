83b62e69d562c9e00265a74e469586cdce05477a
Syltis <krsylte@gmail.com>
2019-09-13 15:51:01 +0200

    Begun login-functionality with Auth0. Currently only reidrects to login-page.

--- fridgein_fe/src/main.js
+++ fridgein_fe/src/main.js
@@ -4,6 +4,12 @@ import App from './App.vue'
 import './../node_modules/jquery/dist/jquery.min.js';
 import VueRouter from 'vue-router';
 import store from './store';
+import auth0 from 'auth0-js/build/auth0';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faFontAwesome } from '@fortawesome/free-brands-svg-icons'
 
 import NavigationBar from "./components/layout/NavigationBar";
 import BottomBar from "./components/layout/BottomBar";
@@ -13,8 +19,14 @@ import RecipePage from "./pages/RecipePage";
 import StartPage from "./pages/StartPage";
 import './registerServiceWorker'
 
+library.add(faUserSecret);
+library.add(faFontAwesome);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
 Vue.config.productionTip = false;
+
 Vue.use(VueRouter);
+
 
 Vue.component('navigationBar', NavigationBar);
 Vue.component('bottomBar', BottomBar);
