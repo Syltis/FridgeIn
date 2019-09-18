537cbd8e068954a9a339106c5031cf1fe819f468
Syltis <krsylte@gmail.com>
2019-09-18 14:35:07 +0200

    Non-authenticated users can now only access the Home and Callback-pages

--- fridgein_fe/src/router/index.js
+++ fridgein_fe/src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
+import Router from 'vue-router';
+import auth from '../auth/authService';
 
 import FoodPage from '../pages/FoodPage';
 import ContactPage from "../pages/ContactPage";
@@ -7,17 +8,29 @@ import RecipePage from "../pages/RecipePage";
 import StartPage from "../pages/StartPage";
 import CallBack from '../components/CallBack';
 
-Vue.use(VueRouter);
+Vue.use(Router);
 
 const routes = [
-    {path: '/', component: StartPage},
-    {path: '/food', component: FoodPage},
-    {path: '/contact', component: ContactPage},
-    {path: '/recipes', component: RecipePage},
-    {path: '/callback', component: CallBack}
+    { path: '/', component: StartPage },
+    { path: '/food', component: FoodPage },
+    { path: '/contact', component: ContactPage },
+    { path: '/recipes', component: RecipePage },
+    { path: '/callback', component: CallBack }
   ];
-  
-  export default new VueRouter({
+
+  const router = new Router({
     mode: 'history',
     routes: routes
   });
+
+  // A non-authenticated user can only access the home(/) and callback-page.
+  router.beforeEach((to, from, next) => {
+    if (to.path === "/" || to.path === "/callback" || auth.isAuthenticated()) {
+      return next();
+    }
+
+
+    auth.login({ target: to.path });
+  });
+
+  export default router;
